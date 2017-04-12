776dd6586fb52067674eaa0fd0df5c7b583d111c
Yura Detkovskiy <9119113142@mail.ru>
2017-04-12 12:20:43 +0300

    на проверку

---
+++ js/data.js
@@ -0,0 +1,127 @@
+/**
+ * Created by Yura on 12.04.17.
+ */
+
+'use strict';
+
+/* запись в глобальную область массива с сгенерированными объявлениями */
+window.data = (function () {
+
+  /* объект с константами */
+  var constantObject = {
+    MIN_RENT_PRICE: 1000,
+    MAX_RENT_PRICE: 1000000,
+    MIN_ROOMS: 1,
+    MAX_ROOMS: 5,
+    MIN_GUESTS: 1,
+    MAX_GUESTS: 10
+  };
+
+  /* объект с информацией о жилье */
+  var infoObject = {
+    arrTitle: [
+    'Большая уютная квартира',
+    'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец',
+    'Маленький ужасный дворец',
+    'Красивый гостевой домик',
+    'Некрасивый негостеприимный домик',
+    'Уютное бунгало далеко от моря',
+    'Неуютное бунгало по колено в воде'
+    ],
+    arrType: [ 'flat', 'house', 'bungalo' ],
+    arrCheckin: [ '12:00', '13:00', '14:00' ],
+    arrCheckout: [ '12:00', '13:00', '14:00' ],
+    arrFeatures: [ 'wifi', 'dishwasher',  'parking', 'washer', 'elevator', 'conditioner' ]
+  };
+
+  /* функция случайного элемента из массива */
+  var getRandomItemArr = function (arr) {
+    var randomNumber = Math.floor(Math.random() * arr.length);
+    return arr[randomNumber];
+  };
+
+  /* функция случайного числа из периода */
+  var getRandomRange = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  var imgIndex = function (i) {
+    if (i === 0) {
+      i = '01';
+    } else {
+      i = (i < 10) ? ('0' + ++i) : i;
+    }
+    return i;
+  };
+
+  /* формирование особенностей жилья */
+  var quantityFeatures = function () {
+    var sizeFeatures = infoObject.arrFeatures.length;
+    var randomNumber = Math.floor(Math.random() * sizeFeatures);
+    sizeFeatures = (randomNumber === 0) ? 1 : randomNumber;
+    var features = [];
+    for (var i = 0; i < sizeFeatures; i++) {
+      features.push(infoObject.arrFeatures[i]);
+    }
+    return features;
+  };
+
+  /* определение типа жилья */
+  var lodgeType = function () {
+    var type = '';
+    var randomType = getRandomItemArr(infoObject.arrType);
+    if (randomType === 'flat') {
+      type = 'Квартира';
+    } else if (randomType === 'bungalo') {
+      type = 'Бунгало';
+    } else {
+      type = 'Дом';
+    }
+    return type;
+  };
+
+  /* функция генирирования координат объявления */
+  var generateAddress = function () {
+    return {x: getRandomRange(300, 900), y: getRandomRange(100, 500)};
+  };
+
+  /* формирование случайного объявления */
+  var renderOneObject = function (i) {
+    var addressObject = generateAddress();
+    return {
+      author: {
+        avatar: 'img/avatars/user' + imgIndex(i) + '.png'
+      },
+      offer: {
+        title: getRandomItemArr(infoObject.arrTitle),
+        address: addressObject.x + ', ' + addressObject.y,
+        price: getRandomRange(constantObject.MIN_RENT_PRICE, constantObject.MAX_RENT_PRICE),
+        type: lodgeType(),
+        rooms: getRandomRange(constantObject.MIN_ROOMS, constantObject.MAX_ROOMS),
+        guests: getRandomRange(constantObject.MIN_GUESTS, constantObject.MAX_GUESTS),
+        checkin: getRandomItemArr(infoObject.arrCheckin),
+        checkout: getRandomItemArr(infoObject.arrCheckout),
+        features: quantityFeatures(),
+        description: ' ',
+        photos: []
+      },
+      location: {
+        x: addressObject.x,
+        y: addressObject.y
+      }
+    };
+  };
+
+  /* создание массива объявлений */
+  var objects = (function () {
+    objects = [];
+    for (var i = 0; i <= 8; i++) {
+      objects.push(renderOneObject(i));
+    }
+    return objects;
+  })();
+
+  return objects;
+
+})();
---
+++ js/pin.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Yura on 12.04.17.
+ */
+'use strict';
+
+(function () {
+
+  /* зазмеры иконки Pin */
+  var sizeIconPin = {
+    width: 56,
+    height: 75
+  };
+
+  /* формирование pin */
+  var generatePin = function (i) {
+    var getX = window.data[i].location.x;
+    var getY = window.data[i].location.y;
+    var div = document.createElement('div');
+    div.className = 'pin';
+    div.setAttribute('style', 'left:' + (getX + (sizeIconPin.width / 2)) + 'px; top:' + (getY + sizeIconPin.height) + 'px;');
+    div.setAttribute('data-index', i);
+
+    var img = document.createElement('img');
+    img.className = 'rounded';
+    img.height = '40';
+    img.width = '40';
+    img.setAttribute('src', window.data[i].author.avatar);
+    img.setAttribute('tabindex', '0');
+    div.appendChild(img);
+
+    return div;
+  };
+
+  /* отрисовка pin в фрагмент */
+  var tokyoPins = document.querySelector('.tokyo__pin-map');
+  var fragment = document.createDocumentFragment();
+  (function () {
+
+    for (var i = 0; i < 8; i++) {
+      var pin = generatePin(i);
+      fragment.appendChild(pin);
+    }
+
+    tokyoPins.appendChild(fragment);
+    return tokyoPins;
+  })();
+
+  /* перестановка pin--active */
+  var changeActivePins = function (item) {
+    delActivePin();
+    item.classList.add('pin--active');
+  };
+
+  /* удаление класса pin--active (используется при закрытии объявления) */
+  var delActivePin = function () {
+    var pinActive = tokyoPins.querySelector('.pin--active');
+    if (pinActive !== null) {
+      pinActive.classList.remove('pin--active');
+    }
+  };
+
+  /* отслеживание нажатия на pin
+   * перестановка класса Active на нажатый pin
+   * открытие объявления по индексу pin */
+  var pins = tokyoPins.querySelectorAll('.pin');
+  changeActivePins(pins[0]);
+  for (var i = 0; i <= 8; i++) {
+
+    pins[i].addEventListener('click', function (evt) {
+      changeActivePins(evt.currentTarget);
+      window.card.openDialog(evt);
+    });
+
+    pins[i].addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 13) {
+        changeActivePins(evt.currentTarget);
+        window.card.openDialog(evt);
+      }
+    });
+  }
+
+})();
